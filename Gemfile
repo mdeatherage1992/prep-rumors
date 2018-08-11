454bcdeceafdd26082f932d6a7d8256c7d6efc01
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-11 15:04:35 +0000

    comment touches

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.10'
+gem 'simple_form'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'acts_as_votable', '~> 0.11.1'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :posts do
   member do
     put "like", to: "posts#upvote"
     put "dislike", to: "posts#downvote"
    end
+   resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:"posts#index"
